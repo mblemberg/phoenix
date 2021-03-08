9776fa55b7663866490fda05ec5bb5515a0e7422
mblemberg <53454719+mblemberg@users.noreply.github.com>
2021-03-07 20:04:47 -0600

    Update 5_Ethereum-ethermine.sh

--- 5_Ethereum-ethermine.sh
+++ 5_Ethereum-ethermine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # start mining
-/home/mike/crypto/PhoenixMiner -pool ssl://us1.ethermine.org:5555 -wal 0x6A06fe985943822553c43C011d8fb0CB15662cAa -worker nimzo -coin eth -proto 3 -tt 70 -tstop 80
+/home/av/phoenix/PhoenixMiner -pool ssl://us1.ethermine.org:5555 -wal 0x3113922d9180Ae4Ed15DdDF654a5aF3477444214 -worker michelangelo -coin eth -proto 3 -tt 70 -tstop 80
